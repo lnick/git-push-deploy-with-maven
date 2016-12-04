30b990f35a98b0de8a56b641581a0d08653c1462
sych74 <sych74@gmail.com>
2016-12-04 19:54:37 +0100

    Create add-deploy-mount.cs

---
+++ scripts/add-deploy-mount.cs
@@ -0,0 +1,16 @@
+//@auth 
+//@req(deployPath, mountTo)
+var mountFrom = "${nodes.build.first.id}";
+var envName = "${settings.targetEnv}".split(".")[0];
+var groups = jelastic.env.control.GetEnvInfo(envName, session).nodeGroups; 
+var mountTo = "cp";
+for (var i = 0; i < groups.length; i++){
+  if (groups[i].name == "storage") {
+    mountTo = "storage";
+    break;
+  }
+}
+
+//resp = jelastic.env.control.AddDockerVolumeByGroup('${env.envName}', session, mountTo, volume); 
+resp = jelastic.env.file.AddMountPointByGroup(envName, session, mountTo, deployPath, 'nfs', null, '/data', mountFrom, '', false); 
+return resp;
