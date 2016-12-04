705adfadd749d3c1b55e28edf862b78e67f2c7ae
sych74 <sych74@gmail.com>
2016-12-04 19:02:45 +0100

    Update redeploy.cs

--- scripts/redeploy.cs
+++ scripts/redeploy.cs
@@ -1,7 +1,15 @@
-//@required('token', 'targetEnv', 'nodeGroup')
+//@req(token, action)
 
 if (token == "${TOKEN}") {
-  return jelastic.env.control.RestartContainersByGroup(targetEnv, signature, nodeGroup); 
+  var targetEnv = "${TARGET_ENV}", nodeGroup = "${NODE_GROUP}";
+  if (action == 'redeploy'){
+    return jelastic.env.control.RestartContainersByGroup(targetEnv, signature, nodeGroup); 
+  } else if (action == 'rebuild'){
+    var nodeId = "${BUILD_NODE_ID}", projectId = "${PROJECT_ID}"; 
+    return jelastic.env.build.BuildProject(targetEnv, signature, nodeId, projectId);
+  } else {
+    return {"result": 3, "error": "unknown action [" + action + "]"}
+  }
 } else {
   return {"result": 8, "error": "wrong token"}
 }
