616abde3d0d2ebb10cbba9d6d37bf6a9c424616d
sych74 <sych74@gmail.com>
2016-12-04 18:48:28 +0100

    Update redeploy.cs

--- scripts/redeploy.cs
+++ scripts/redeploy.cs
@@ -1,7 +1,7 @@
-//@required('token', 'targetAppid', 'nodeGroup')
+//@required('token', 'targetEnv', 'nodeGroup')
 
 if (token == "${TOKEN}") {
-  return jelastic.env.control.RestartContainersByGroup(targetAppid, signature, nodeGroup); 
+  return jelastic.env.control.RestartContainersByGroup(targetEnv, signature, nodeGroup); 
 } else {
   return {"result": 8, "error": "wrong token"}
 }
