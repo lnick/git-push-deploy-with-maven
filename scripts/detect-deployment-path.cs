78542960b09094ede95e50c224535a5a620126c4
sych74 <sych74@gmail.com>
2017-02-11 17:37:30 +0200

    Update detect-deployment-path.cs

--- scripts/detect-deployment-path.cs
+++ scripts/detect-deployment-path.cs
@@ -1,11 +1,28 @@
-var cmd = "m=/etc/jelastic/metainf.conf; e=/etc/jelastic/environment; l=/var/lib/jelastic/libs/envinfo.lib; [ -f $m ] && { source $m; [ -f $e ] && source $e || source $l; }; echo ${WEBROOT:-$Webroot_Path}";
 
-var envName = "${settings.targetEnv}".split(".")[0];
-var id = "${nodes.cp.first.id}";
-
+//detecting 
 var pathTo = "${settings.deployPathCustom}";
 if ("${settings.deployPath}" == "auto") {
-    var resp = jelastic.env.control.ExecCmdById(envName, session, id, toJSON([{
+    
+    //looking for first node in cp layer 
+    var group = "cp";
+    var nodeId = -1; 
+    var envName = "${settings.targetEnv}".split(".")[0];
+    var resp = jelastic.env.control.GetEnvInfo(envName, session);
+    if (resp.result != 0) return resp;
+    var nodes = resp.nodes;
+    for (var i = 0; i < nodes.length; i++){
+        if (nodes[i].nodeGroup == group) {
+            nodeId = nodes[i].id;
+            break; 
+        }
+    }
+    if (nodeId == -1){
+        return {result: 99, error: "zero nodes in group [" + group + "]"}
+    }
+
+    //getting WEBROOT as deployment path
+    var cmd = "m=/etc/jelastic/metainf.conf; e=/etc/jelastic/environment; l=/var/lib/jelastic/libs/envinfo.lib; [ -f $m ] && { source $m; [ -f $e ] && source $e || source $l; }; echo ${WEBROOT:-$Webroot_Path}";
+    var resp = jelastic.env.control.ExecCmdById(envName, session, nodeId, toJSON([{
         "command": cmd
     }]), true, "root");
     if (resp.result != 0) return resp;
